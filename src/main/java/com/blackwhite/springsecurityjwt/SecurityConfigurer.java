510ba29eacb3bca1daba4b555ad7e3c130d1891c
NitichaiAom <NitichaiSawangsai@gmail.com>
2021-12-01 23:35:56 +0700

    fix: fixed auth api

--- src/main/java/com/blackwhite/springsecurityjwt/SecurityConfigurer.java
+++ src/main/java/com/blackwhite/springsecurityjwt/SecurityConfigurer.java
@@ -4,6 +4,7 @@ import com.blackwhite.springsecurityjwt.services.MyUserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,12 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
        .authorizeRequests().antMatchers("/authenticate").permitAll()
        .anyRequest().authenticated();
 
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
